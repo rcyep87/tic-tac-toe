9521894baed24e242032718f03bc0921e01e12ce
Ryan Yep <ryan.yep@gmail.com>
2015-08-29 15:56:48 -0500

    v2.0 user prompt and ability to access updated gameboard array to be used for comparison with game logic

--- bin/tic-tac-toe.rb
+++ bin/tic-tac-toe.rb
@@ -2,8 +2,10 @@
 
 class GameCard
 
+  attr_accessor :gameboard
+
   def initialize
-    @gamecard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     @x_or_o
   end
 
@@ -11,30 +13,30 @@ class GameCard
     puts "------------------------------------------------"
     puts "|              |                |              |"
     puts "|              |                |              |"
-    puts "|      #{@gamecard[0]}       |        #{@gamecard[1]}       |       #{@gamecard[2]}      |"
+    puts "|      #{@gameboard[0]}       |        #{@gameboard[1]}       |       #{@gameboard[2]}      |"
     puts "|              |                |              |"
     puts "|              |                |              |"
     puts  "-----------------------------------------------"
     puts "|              |                |              |"
     puts "|              |                |              |"
-    puts "|      #{@gamecard[3]}       |        #{@gamecard[4]}       |       #{@gamecard[5]}      |"
+    puts "|      #{@gameboard[3]}       |        #{@gameboard[4]}       |       #{@gameboard[5]}      |"
     puts "|              |                |              |"
     puts "|              |                |              |"
     puts  "-----------------------------------------------"
     puts "|              |                |              |"
     puts "|              |                |              |"
-    puts "|      #{@gamecard[6]}       |        #{@gamecard[7]}       |       #{@gamecard[8]}      |"
+    puts "|      #{@gameboard[6]}       |        #{@gameboard[7]}       |       #{@gameboard[8]}      |"
     puts "|              |                |              |"
     puts "|              |                |              |"
     puts  "-----------------------------------------------"
   end
 
   def user_input_on_board(index, x_or_o)
-    @gamecard[index] = x_or_o.to_s.upcase
+    @gameboard[index] = x_or_o.to_s.upcase
   end
 
   def array_length
-    @gamecard.length
+    @gameboard.length
   end
 
 end
@@ -51,73 +53,46 @@ class Player
       puts "Please choose an (X) or (O) to play, Player1! > "
       x_or_o = gets.chomp.upcase
 
-    if x_or_o != ("X" || "O")
+    while x_or_o != ("X" || "O")
       puts "Please actually choose an (X) or (O) to play, Player1! > "
       x_or_o = gets.chomp.upcase
-    elsif x_or_o = ("X" || "O")
-      puts "Would you like to be (X) or (O) today, Player 1 > ?"
-      x_or_o = gets.chomp.upcase # include everything in the while loop below
     end
 
     x = 0
-    while x < 8
+    while x < 8 && x >= 0
       if player_input.array_length > 9 # makes sure you're playing within the matrix
         puts "pick another spot to play on! (0..8)"
         player_input.gamecard
         puts "Where would you like to play? (Choose between 0 and 8) > "
         place = gets.chomp.to_i
         player_input.user_input_on_board(place, x_or_o)
+        puts gamecard
         x += 1
       else
         player_input.gamecard
         puts "Where would you like to play? (Choose between 0 and 8) > "
         place = gets.chomp.to_i
         player_input.user_input_on_board(place, x_or_o)
-        # player_input.gamecard
+        # puts player_input.gameboard - spits out updated gameboard array
         x += 1
       end
     end
   end
 end
 
-# count = GameCard.new
-# puts count.array_length Communicates length of array
+class Game
+
+  def initialize
+  end
+
+  def winning_combo
+    choice = GameCard.new
+    puts choice.choice_array
+  end
+
+end
 
 player1 = Player.new
 player1.player_prompt
-
-
-# # user input
-# player_input = GameCard.new
-# puts "Would you like to be (X) or (O) today, Player 1 > ?"
-# x_or_o = gets.chomp
-
-# puts "Where would you like to play? (Choose between 1 and 9) > "
-# place = gets.chomp.to_i
-# player_input.user_input_on_board(place, x_or_o)
-# player_input.gamecard
-
-# puts "Where would you like to play? (Choose between 1 and 9) > "
-# place = gets.chomp.to_i
-# player_input.user_input_on_board(place, x_or_o)
-# player_input.gamecard
-
-# puts "Where would you like to play? (Choose between 1 and 9) > "
-# place = gets.chomp.to_i
-# player_input.user_input_on_board(place, x_or_o)
-# player_input.gamecard
-
-# if (player_input.gamecard[1] && player_input.gamecard[2] && player_input.gamecard[3] == "X")
-#   player_input.gamecard
-#   puts "We have a winner!"
-# else
-#   puts "Keep playing!"
-# end
-
-# when (player_input.gamecard[1] && player_input.gamecard[2] && player_input.gamecard[3] == "X") || (player_input.gamecard[1] && player_input.gamecard[2] && player_input.gamecard[3] == "O")
-#   puts "We have a winner!"
-# else
-#   puts "Keep playing!"
-# end
 
  # binding.pry
